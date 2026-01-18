e1c2f8e834473a498da44b9def3a7362e39ffade
mforbeskr <your@email.com>
2026-01-18 01:07:57 +0100

    fix n tests

--- src/aRun/Main.java
+++ src/aRun/Main.java
@@ -2,12 +2,6 @@ package aRun;
 
 import pack1Brewery.Ale;
 import pack1Brewery.Beer;
-import pack1Brewery.Lager;
-import pack2HospitalManagement.Anesthesiologist;
-import pack2HospitalManagement.Doctor;
-import pack2HospitalManagement.Orthopedic;
-import pack4MusicStreaming.Playlist;
-import pack4MusicStreaming.Song;
 
 public class Main
 {
